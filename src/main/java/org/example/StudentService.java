7205830e660f0a2baab7c2260729852ca4ba56b6
Md Redwan Hasan <redwanhasan980@gmail.com>
2026-05-06 21:16:40 +0600

    Assert Throws

--- src/main/java/org/example/StudentService.java
+++ src/main/java/org/example/StudentService.java
@@ -11,4 +11,18 @@ public class StudentService {
     public void addStudent(Students student) {
         this.studentslist.add(student);
     }
+    public Students getStudentById(int id)
+    {
+        return studentslist.stream()
+                .filter(students -> students.getId()==id)
+                .findFirst()
+                .orElse(null);
+    }
+    public Students getStudentByName(String name)
+    {
+       return studentslist.stream()
+                .filter((students )-> students.getName().equals(name))
+                .findFirst()
+                .orElseThrow(()-> new StudentNotFoundException("Student not Found" + name));
+    }
 }
--- src/test/java/org/example/FactorialTest.java
+++ src/test/java/org/example/FactorialTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class FactorialTest {
     @Test
     void factorialtest()
@@ -11,5 +13,12 @@ public class FactorialTest {
         Factorial f1 = new Factorial();
         int r=f1.factorial(5);
         assertEquals(120,r);
+    }
+    @Test
+    void factorialTest2()
+    {
+         Factorial f1 = new Factorial();
+         int res=f1.factorial(5);
+         assertNotEquals(120,res);
     }
 }
