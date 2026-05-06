0fd33f1caf953bef8d2a5c393991ac93a5c532b8
Md Redwan Hasan <redwanhasan980@gmail.com>
2026-05-06 21:16:40 +0600

    Assert True and False

--- src/test/java/org/example/StudentServiceTest.java
+++ src/test/java/org/example/StudentServiceTest.java
@@ -15,13 +15,21 @@ class StudentServiceTest
           Students s1 = new Students(5, "Nirob");
 
          List<Students> testStudentList = studentService.getStudentslist();
+         studentService.addStudent(s1);
          boolean actualResult = testStudentList.isEmpty();
+         //assertTrue(actualResult);
+//         assertTrue(()->actualResult);
+       // assertTrue(actualResult,"There are students");
          assertTrue(actualResult);
      }
      @Test
-     void setStudentTest()
+     void getStudentTestFalse()
      {
-
+       StudentService studentService = new StudentService();
+       Students s2 =new Students(10,"jawad");
+      // studentService.addStudent(s2);
+       boolean actualResult= studentService.getStudentslist().isEmpty();
+       assertFalse(actualResult);
      }
 
 }
